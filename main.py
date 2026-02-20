9947b991adbe6263123ffcdc02f948d2c9896a35
LuizPk13 <fernando.adr13pk@gamil.com>
2026-02-19 23:19:11 -0300

    Modificando arquivo main.py a partir da develop

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib as plt
+
+# digitando no terminal:
+    # git checkout -b "develop"
+# Serve para criar uma cópia do arquivo original e trabalhar nele sem atrapalhar os coleguinhas
+
+print('Trabalhando na develop')
